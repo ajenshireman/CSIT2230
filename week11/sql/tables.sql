cb718cfbf12e9439af9415999b841073e61248e8
ajenshireman <awshireman@pstcc.edu>
2013-11-16 11:34:55 -0500

    updated table creation scripts

--- week11/sql/tables.sql
+++ week11/sql/tables.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` BIGINT(20) UNSIGNED NOT NULL,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `joindate` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username` (`username` ASC),
-  INDEX `email` (`email` ASC))
-ENGINE = InnoDB;
+CREATE TABLE `user` (
+    `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `joindate` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id` (`id`),
+    UNIQUE INDEX `username` (`username`),
+    INDEX `email` (`email`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB;
