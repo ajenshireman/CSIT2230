bd8c17db4e4f2537a96ae8043c2074f63c498d57
ajenshireman <awshireman@pstcc.edu>
2013-11-18 19:00:41 -0500

    web bug code

---
+++ week12/logo.gif/index.php
@@ -0,0 +1,38 @@
+<?php
+$db = new mysqli('localhost', 'username', 'password', 'database');
+ 
+if (mysqli_connect_errno()) {
+ //echo 'Error: Could not connect to the database..';
+ exit;
+}
+ 
+if (!empty($_SERVER['REMOTE_ADDR'])) {
+ $ip = $_SERVER['REMOTE_ADDR'];
+ $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+ $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+ $agent = $_SERVER["HTTP_USER_AGENT"];
+ $date = date("Y-m-d",time());
+ $time = date("H:i:s",time());
+ 
+ $sql = "INSERT INTO stats (ip,host,referer,agent,date,time) 
+ VALUES ('$ip','$host','$referer','$agent', '$date','$time');";
+ 
+ //echo "$sql<br />";
+ 
+ if (!$db->query($sql)) {
+    //echo $db->error;
+ }
+	
+ $db->close();
+}
+/*
+header( 'Content-type: image/gif' );
+echo chr(71).chr(73).chr(70).chr(56).chr(57).chr(97).
+chr(1).chr(0).chr(1).chr(0).chr(128).chr(0).
+chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).
+chr(33).chr(249).chr(4).chr(1).chr(0).chr(0).
+chr(0).chr(0).chr(44).chr(0).chr(0).chr(0).chr(0).
+chr(1).chr(0).chr(1).chr(0).chr(0).chr(2).chr(2).
+chr(68).chr(1).chr(0).chr(59);
+*/
+?>
