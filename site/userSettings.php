9d06c964728357198599a4c3c8faf7aef1770750
ajenshireman <ajenshireman@gmail.com>
2013-11-25 09:30:20 -0500

    deleteAccount pane and form

--- site/userSettings.php
+++ site/userSettings.php
@@ -20,6 +20,7 @@ require_once 'includes/head.php';
                         <li class="nav nav-pill nav-stacked"><a class="swap" data-target="details" href="#">Account Details</a></li>
                         <li class="nav nav-pill nav-stacked"><a class="swap" data-target="changeEmail"href="#">Change e-mail</a></li>
                         <li class="nav nav-pill nav-stacked"><a class="swap" data-target="changePassword" href="#">Change Password</a></li>
+                        <li class="nav nav-pill nav-stacked"><a class="swap" data-target="deleteAccount" href="#">Delete Account</a></li>
                     </ul>
                 </nav>
                 <div class="col-xs-12 col-sm-9">
@@ -87,6 +88,42 @@ require_once 'includes/head.php';
                                 <button class="btn btn-cancel btn-inline" name="btnCancelPassword" value="Cancel">Cancel</button>
                             </div>
                         </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="deleteAccount" class="form-swap" hidden>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">Delete Account</h4>
+                            </div>
+                            <div class="panel-body">
+                                <div class="alert alert-danger">
+                                    THIS CANNOT BE UNDONE
+                                </div>
+                                <div id="confirmDeleteAccount">
+                                    <div class="form-group">
+                                        Do you really want to delete your acount?
+                                    </div>
+                                    <div class="form-group">
+                                        <button id="btnConfirmDeleteAccount" class="btn btn-inline" name="btnConfirmDeleteAccount" value="Yes">Delete</button>
+                                        <button id="btnCancelDeleteAccount" class="btn btn-inline" name="btnCancelDeleteAccount" value="Cancel">Cancel</button>
+                                    </div>
+                                </div>
+                                <form  id="deleteAccountForm" action="deleteAccount.php" method="post" hidden>
+                                    <div class="form-group">
+                                        <div id="deleteAccountError" class="message error alert"></div>
+                                        <input type="password" class="form-control" name="deletePassword" placeholder="Password" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="checkbox" class="pwToggle" />
+                                        <span>Show Password</span>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <button class="btn btn-submit btn-inline" name="btnDeleteAccount" value="Yes">Delete</button>
+                                        <button class="btn btn-cancel btn-inline" name="btnCancelDelete" value="Cancel">Cancel</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
