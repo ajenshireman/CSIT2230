149a89cd828edf680a69181959d455b9d5b69c64
ajenshireman <awshireman@pstcc.edu>
2013-12-02 14:51:42 -0500

    replaced dummy info with span.details

--- site/collections.php
+++ site/collections.php
@@ -57,11 +57,11 @@ require_once 'includes/head.php';
 			<div class="modal-body">
 			    <div class="row itemInfo">
 				<div class="col-xs-12 col-sm-4 col-md-3 text-center"> 
-				    <img src="img/covers/300/captainAmericaTheFirstAvenger_300.jpg" class="itemImage" />
+				    <img class="itemImage" src="" />
 				</div>
 				<div class="col-xs-12 col-sm-8 col-md-9">
 				    <div class="col-xs-12 itemInfo-header"><!--8 col-sm-9 col-md-10">-->
-					<h4>Movie Title (Year)</h4>
+					<h4><span id="title"></span> (<span id="year"></span>)</h4>
 				    </div>
 				    <div class="col-xs-12 itemInfo-header">
 					<button type="button" class="btn btn-default btn-collection" id="rating">
@@ -77,13 +77,13 @@ require_once 'includes/head.php';
 				    </div>
 				    <div class="col-xs-12 itemDetails" id="details">
 					<ul class="list-group">
-					    <li class="list-group-item" id="description">Description: Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</li>
-					    <li class="list-group-item" id="format">Format: DVD, Blu-ray, Digital</li>
-					    <li class="list-group-item" id="studio">Studio:</li>
-					    <li class="list-group-item" id="prducer">Producer:</li>
-					    <li class="list-group-item" id="director">Director:</li>
-					    <li class="list-group-item" id="actors">Starring:</li>
-					    <li class="list-group-item" id="genre">Genre</li>
+					    <li class="list-group-item" id="description">Description: <span class="details"></span></li>
+					    <li class="list-group-item" id="format">Format: <span class="details"></span></li>
+					    <li class="list-group-item" id="studio">Studio: <span class="details"></span></li>
+					    <li class="list-group-item" id="prducer">Producer: <span class="details"></span></li>
+					    <li class="list-group-item" id="director">Director: <span class="details"></span></li>
+					    <li class="list-group-item" id="actors">Starring: <span class="details"></span></li>
+					    <li class="list-group-item" id="genre">Genre: <span class="details"></span></li>
 					    <li class="list-group-item" id="tags">[Action] [Super Heroes] [Comic Book] [Avengers]</li>
 					</ul>
 				    </div>
