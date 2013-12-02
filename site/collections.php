b1a1b143a71f8afcd3e452757859cf8179a89e62
ajenshireman <awshireman@pstcc.edu>
2013-12-02 14:27:37 -0500

    New info modal

--- site/collections.php
+++ site/collections.php
@@ -57,34 +57,16 @@ require_once 'includes/head.php';
 			<div class="modal-body">
 			    <div class="row itemInfo">
 				<div class="col-xs-12 col-sm-4 col-md-3 text-center"> 
-				    <img src="img/covers/300/captainAmericaTheFirstAvenger_300.jpg" class="itemImage" />
+				    <img class="itemImage" src="" />
 				</div>
 			    <div class="col-xs-12 col-sm-8 col-md-9">
 				<div class="col-xs-12 itemInfo-header"><!--8 col-sm-9 col-md-10">-->
-				    <h4><span id="title"></span> (<span id ="year"></span>)</h4>
-				</div>
-				<div class="col-xs-12 itemInfo-header">
-				    <button type="button" class="btn btn-default btn-collection" id="rating">
-					<span class="glyphicon glyphicon-star"> </span>
-					<span class="glyphicon glyphicon-star"> </span>
-					<span class="glyphicon glyphicon-star"> </span>
-					<span class="glyphicon glyphicon-star"> </span>
-					<span class="glyphicon glyphicon-star"> </span>
-				    </button>
-				    <button type="button" class="btn btn-default btn-collection" id="favorite">
-					<span class="glyphicon glyphicon-thumbs-up"> </span>
-				    </button>
+				    <h4><span id="title"></span>)</h4
 				</div>
 				<div class="col-xs-12 itemDetails" id="details">
 				    <ul class="list-group">
 					<li class="list-group-item" id="description">Description: <span class="details"></span></li>
-					<li class="list-group-item" id="format">Format: <span class="details"></span></li>
-					<li class="list-group-item" id="studio">Studio: <span class="details"></span></li>
-					<li class="list-group-item" id="prducer">Producer: <span class="details"></span></li>
-					<li class="list-group-item" id="director">Director: <span class="details"></span></li>
-					<li class="list-group-item" id="actors">Starring: <span class="details"></span></li>
-					<li class="list-group-item" id="genre">Genre <span class="details"></span></li>
-					<li class="list-group-item" id="tags"><span class="details"></span></li>
+					<li class="list-group-item" id="mimeType">MIME-type: <span class="details"></span></li>
 				    </ul>
 				</div>
 			    </div>
