91ad21a26e33594dce3a89c64a2e47186ef4e968
ajenshireman <awshireman@pstcc.edu>
2013-12-02 13:13:59 -0500

    Put collection name in title directly

--- site/js/collections.js
+++ site/js/collections.js
@@ -3,21 +3,22 @@ $(function(){
     $('nav').on('click', '.collection-nav', function(e){
         e.preventDefault();
         var collectionID = $(this).attr('data-collection_id');
+        var collectionName = $(this).html();
+        $('#collectionName').html(collectionName);
         $.post('collection.php', { collectionID: collectionID }, function(data){
-            $('#collectionError').html(data).show();
-            return;
+            //$('#collectionError').html(data).show();
+            //return;
             var result = $.parseJSON(data);
             var errorClass = '';
             if ( result.success == 'true') {
                 errorClass = 'alert-success';
                 $('#collectionError').html(result.message).removeClass('alert-warning alert-success').addClass(errorClass).hide();
-                $('#collectionName').html(result.collectionName);
+                //$('#collectionName').html(result.collectionName);
                 $('#collectionGrid').html(result.output);
             } else {
                 errorClass = 'alert-warning';
                 $('#collectionError').html(result.message).removeClass('alert-warning alert-success').addClass(errorClass).show();
             }
-            
         });
     });
 });
