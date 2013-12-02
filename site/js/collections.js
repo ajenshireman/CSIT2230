37d5b95113cacc7bdddd15d2d13987672b420b0d
ajenshireman <ajenshireman@gmail.com>
2013-12-02 11:24:02 -0500

    js for shoeing collections

--- site/js/collections.js
+++ site/js/collections.js
@@ -1,4 +1,24 @@
 $(function(){
-    
+    /* Navigation */
+    $('nav').on('click', '.collection-nav', function(e){
+        e.preventDefault();
+        var collectionID = $(this).attr('data-collection_id');
+        $.post('collection.php', { collectionID: collectionID }, function(data){
+            alert(data);
+            return;
+            var result = $.parseJSON(data);
+            var errorClass = '';
+            if ( result.success == 'true') {
+                errorClass = 'alert-success';
+                $('#collectionError').html(result.message).removeClass('alert-warning alert-success').addClass(errorClass).hide();
+                $('#collectionName').html(result.collectionName);
+                $('#collectionGrid').html(result.output);
+            } else {
+                errorClass = 'alert-warning';
+                $('#collectionError').html(result.message).removeClass('alert-warning alert-success').addClass(errorClass).show();
+            }
+            
+        });
+    });
 });
 
