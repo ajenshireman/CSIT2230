8b089227dfb0d2633f6e75defaf9fab9864c401c
ajenshireman <awshireman@pstcc.edu>
2013-12-02 18:37:55 -0500

    Removed the () from year

--- site/collections.php
+++ site/collections.php
@@ -63,7 +63,7 @@ require_once 'includes/head.php';
 				</div>
 				<div class="col-xs-12 col-sm-8 col-md-9">
 				    <div class="col-xs-12 itemInfo-header"><!--8 col-sm-9 col-md-10">-->
-					<h4><span id="title"></span> (<span id="year"></span>)</h4>
+					<h4><span id="title"></span></h4>
 				    </div>
 				    <div class="col-xs-12 itemDetails" id="details">
 					<ul class="list-group">
